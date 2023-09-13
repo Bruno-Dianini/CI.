8e922ab5902a1e76d67a59673ec40765984e71c5
Bruno Dianini <bruno.dianini@kumulus.com>
2023-09-13 15:27:46 -0300

    add docker

--- src/test/java/sample/quarkus/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package sample.quarkus;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
